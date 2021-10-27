1a1fa5366f6ef9eae4dbe0f9687050a991cc6ce0
MyungWanPark <mywanpark@gmail.com>
2021-10-27 17:51:59 +0900

    JS: scroll to menu item

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const navbar = document.querySelector("#navbar");
+const navbarMenu = document.querySelector(".navbar__menu");
 
 // navbar's background change with scroll height
 document.addEventListener("scroll", () => {
@@ -9,4 +10,12 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// scroll to the tapped menu
+navbarMenu.addEventListener("click", (event) => {
+  const selectedMenuElem = event.target;
+  const destID = selectedMenuElem.dataset.link;
+  const destElem = document.querySelector(destID);
+  destElem.scrollIntoView({ behavior: "smooth" });
 });
