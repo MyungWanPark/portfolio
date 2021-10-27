581038bd8b7ff21a4aedc1cd5d742b5a90caef23
MyungWanPark <mywanpark@gmail.com>
2021-10-27 19:25:42 +0900

    JS: home scroll opacity control

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 const navbar = document.querySelector("#navbar");
 const navbarMenu = document.querySelector(".navbar__menu");
+const contactBtn = document.querySelector(".home__contact");
+const homeContainer = document.querySelector(".home__container");
 
 // navbar's background change with scroll height
 document.addEventListener("scroll", () => {
-  if (window.scrollY > navbar.offsetHeight) {
+  if (window.scrollY > navbar.getBoundingClientRect().height) {
     navbar.classList.add("navbar--dark");
   } else {
     navbar.classList.remove("navbar--dark");
@@ -16,6 +18,22 @@ document.addEventListener("scroll", () => {
 navbarMenu.addEventListener("click", (event) => {
   const selectedMenuElem = event.target;
   const destID = selectedMenuElem.dataset.link;
-  const destElem = document.querySelector(destID);
+  scrollIntoViews(destID);
+});
+
+// when contact me is clicked, scroll to the buttom
+contactBtn.addEventListener("click", () => {
+  scrollIntoViews("#contact");
+});
+
+function scrollIntoViews(selector) {
+  const destElem = document.querySelector(selector);
   destElem.scrollIntoView({ behavior: "smooth" });
+}
+
+// When scroll down, home content fade away
+document.addEventListener("scroll", () => {
+  const opacitySlope =
+    window.scrollY / homeContainer.getBoundingClientRect().height;
+  homeContainer.style.opacity = 1 - opacitySlope;
 });
